56001697bed158ce21a0c9ff0026d0af6403d278
Akrem Toum <akremtoum54@gmail.com>
2025-11-25 13:39:54 +0000

    add postgress service and DB connection check

--- src/app.py
+++ src/app.py
@@ -1,10 +1,26 @@
 from flask import Flask, jsonify
 
+from database import get_db_connection
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return jsonify({"message": "Welcome to Cloud Café CI/CD v2 ☕"})
+
+@app.route("/db-check")
+def db_check():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT 1;")
+        result = cur.fetchone()
+        cur.close()
+        conn.close()
+        return jsonify({"db": "ok", "result": result[0]})
+    except Exception as e:
+        return jsonify({"db": "error", "details": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
---
+++ src/database.py
@@ -0,0 +1,16 @@
+import os
+
+import psycopg2
+from dotenv import load_dotenv
+
+# Load variables from .env inside the container
+load_dotenv()
+
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST", "db"),
+        port=int(os.getenv("DB_PORT", "5432")),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+    )
